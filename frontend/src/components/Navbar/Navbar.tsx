183260f63d41c97321fe9ed962e73dc9424d1ec7
Дмитрий <Avgustinovich.DV@parmalogica.ru>
2024-05-12 19:21:23 +0500

    =

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,9 +1,11 @@
+import React from "react";
+
 import {
   InboxOutlined,
   MenuOutlined,
   ShoppingCartOutlined,
 } from "@ant-design/icons";
-import { Badge, Button, Input, Typography } from "antd";
+import { Badge, Button, Drawer, Input, Typography } from "antd";
 import { Link } from "react-router-dom";
 
 import { DropdownUser } from "components/DropdownUser/DropdownUser";
@@ -16,12 +18,41 @@ import { useSearchProducts } from "hooks/products/use-search-products";
 import styles from "./Navbar.module.scss";
 
 export const Navbar = () => {
+  const [isOpenMenuDrawer, setIsOpenMenuDrawer] = React.useState(false);
+  const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
+
   const { cartProductsData } = useCartActions();
   const { handleSearchProducts } = useSearchProducts();
 
-  return (
-    <div className={styles.wrapper}>
-      <Link to={RouterPath.catalog}>
+  const handleOpenDrawer = () => {
+    setIsOpenMenuDrawer(true);
+  };
+
+  const handleCloseDrawer = () => {
+    setIsOpenMenuDrawer(false);
+  };
+
+  const handleWindowResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  React.useEffect(() => {
+    if (windowWidth > 760 && isOpenMenuDrawer) {
+      handleCloseDrawer();
+    }
+  }, [windowWidth, isOpenMenuDrawer]);
+
+  const desktopMenu = (
+    <div className={styles.desktopMenu}>
+      <Link to={RouterPath.catalog} onClick={handleCloseDrawer}>
         <Button type="primary">
           <MenuOutlined />
           Каталог
@@ -34,7 +65,7 @@ export const Navbar = () => {
         onSearch={handleSearchProducts}
       />
 
-      <div className={styles.icons}>
+      <div className={styles.icons} onClick={handleCloseDrawer}>
         <Link className={styles.iconWrapper} to={RouterPath.orders}>
           <ShoppingCartOutlined className={styles.icon} />
           <Typography.Text>Заказы</Typography.Text>
@@ -52,8 +83,34 @@ export const Navbar = () => {
           <Typography.Text>Корзина</Typography.Text>
         </Link>
       </div>
-
-      <DropdownUser />
     </div>
+  );
+
+  const mobileMenu = <div className={styles.mobileMenu}>{desktopMenu}</div>;
+
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <Button
+          className={styles.openMenuDrawerButton}
+          type="primary"
+          onClick={handleOpenDrawer}
+        >
+          Меню
+        </Button>
+
+        {desktopMenu}
+        <DropdownUser />
+      </div>
+
+      <Drawer
+        title="Меню"
+        placement="left"
+        onClose={handleCloseDrawer}
+        open={isOpenMenuDrawer}
+      >
+        {mobileMenu}
+      </Drawer>
+    </>
   );
 };
